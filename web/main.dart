410ed66f1b70f956f5bfee78bcec3f706cd3ecc1
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2017-09-04 16:53:32 +0200

    first release

---
+++ web/main.dart
@@ -0,0 +1,8 @@
+library simple_example_client;
+
+import 'package:html5/html.dart';
+import 'package:polymerize_common/init.dart';
+import 'package:simple_example/my_tag.dart';
+
+@entryPoint
+void main(){}
