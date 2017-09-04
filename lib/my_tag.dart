915a0ab8c20861a4acaf19c1d93c202e5f97ab40
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2017-09-04 17:08:27 +0200

    little simplification

--- lib/my_tag.dart
+++ lib/my_tag.dart
@@ -1,18 +1,12 @@
-library components;
-
 import 'package:html5/html.dart';
 
 import 'package:polymer_element/polymer_element.dart';
 
 @PolymerRegister('my-tag', template: 'my_tag.html')
 abstract class MyTag extends PolymerElement {
-  external register();
 
   MyTag() {
     print("HELLO THERE !");
   }
 
-  connectedCallback() {
-    super.connectedCallback();
-  }
 }
--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,3 @@
-library simple_example_client;
-
 import 'package:html5/html.dart';
 import 'package:polymerize_common/init.dart';
 import 'package:simple_example/my_tag.dart';
